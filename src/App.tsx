6623ec5704174a296ed5a3bffeffc191378d64d8
Alexandr Radionov <fokk190p@gmail.com>
2024-11-08 18:38:07 +0200

    Change App component

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="custom-text">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="read-docs">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum voluptatibus voluptates enim, ut architecto, quia laborum itaque distinctio fuga quae quasi ratione doloribus? Non dolor consequuntur ipsam porro aperiam, exercitationem ratione debitis perspiciatis dolorem consectetur ea eligendi sunt incidunt provident facilis atque accusantium quia.
       </p>
     </>
   )
